332cbbb9f20c6ff4d7e336539ca570bb0ac4d82d
OtienoSamwel <otienosamwel135@gmail.com>
2020-10-25 17:11:32 +0300

    added database functionality

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 from flask_mail import Mail, Message
+#import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+#set the env to 'PROD' before merge in the main for CI
+ENV = 'DEV'
+
+if ENV == 'DEV':
+    app.debug = True
+    #configure the database localy for testing
+    app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql://root:yourpassword@localhost/database_name'
+else:
+    #run the online db (uncomment it first)
+    app.debug = False
+   # app.config['SQLALCHEMY_DATABASE_URI'] =  #paste the url for the database from your heroku account after creating it
+
+# diable mofication tracking
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# initialize the database
+db = SQLAlchemy(app)
+
+
+class Emails(db.Model):
+    __table_name__ = 'emails'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(200), unique= True)
+
+    def __init__(self,email):
+        self.email = email
+
 
 app.config['DEBUG'] = True
 app.config['TESTING'] = False
@@ -23,6 +53,24 @@ mail = Mail(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+#create a submit route
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        email = request.form['email']
+        print(email)
+        if email == '':
+            return render_template('index.html', message = 'This field cannot be empty!')
+
+            #ensure no duplicate emails to avoid db errors
+        if db.session.query(Emails).filter(Emails.email==email).count() == 0:
+            data = Emails(email)
+            db.session.add(data)
+            db.session.commit()
+            print("yes")
+            return render_template('index.html', message2='You have successfully subscribed to our mailing list!')
+        return render_template('index.html')
 
 
 @app.route("/mail")
